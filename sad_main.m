e38f715b88e235aad9c562c521ec46f5cc7048f8
Insanfirdaus <i.firdaus@vu.nl>
2019-10-22 23:05:16 +0200

    update data structure

--- sad_main.m
+++ sad_main.m
@@ -1,5 +1,5 @@
 function [fullpath, output] = sad_main()
-    rootDataPath = 'SAD/data_lemon/MRI_PERPHYS_joined:';
+    rootDataPath = 'data_lemon';
     subjectID = 'sub-010003:/ses-01:/func:';
 %     filePath = 'sub-010003_ses-01_task-rest_acq-AP_run-01_recording-ecg_physio.tsv';
     filePath = 'short.tsv';
